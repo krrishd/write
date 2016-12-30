69a5875953a31fedb6108cf6e2791b4e41b6a908
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-12-30 16:53:57 -0700

    Reverting past two commits

--- src/configure-localStore.js
+++ src/configure-localStore.js
@@ -1,9 +1,6 @@
 import LocalStore from './localStore';
 
 let store = (name, existing) => {
-  if ((typeof existing) == 'string') {
-    return new LocalStore(name, JSON.parse(existing));
-  }
   return new LocalStore(name, existing);
 };
 
