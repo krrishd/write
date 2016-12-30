855bf66c51537fd13fc0c19c39d338ce7176c8bf
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-12-30 14:09:42 -0700

    Added GitHub to about page

--- src/About.js
+++ src/About.js
@@ -22,7 +22,7 @@ class About extends Component {
           <p>This is a a writing app based on <a target="_blank" href="http://www.hailoverman.com/flowstate">Flowstate</a>.</p>
           <p>The purpose of this is to get the user to write for a set duration without any inhibition or distraction allowed. To achieve this, the editor lets one write for a specified duration, within which, if the user goes inactive, all that has been written is deleted completely. This forces the user to continuously write throughout the session, resulting in a rough draft of (a lot of) content that can then be revised/polished.</p>
           <p>All the functionality is client-side; it's a single-page app built with React and all saved data is only saved in localStorage.</p>
-          <a href="//https://github.com/krrishd/write" className="extLink">/github</a>
+          <a href="https://github.com/krrishd/write" className="extLink">/github</a>
         </div>
       </div>
     );
