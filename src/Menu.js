5e445aa4d238c1979451a1a9d8fc16dc3e3ef3b9
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-12-31 01:11:49 -0700

    Made menu more interactive + added a default

--- src/Menu.js
+++ src/Menu.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
 
 import {
-  Link
+  Link,
+  browserHistory
 } from 'react-router';
 
 import NProgress from 'nprogress';
 
 class Menu extends Component {
-  componentDidMount() {
-    document.title = "Write";
-    NProgress.done();
+  constructor(props) {
+    super(props);
+    this.indexToURL = [
+      'fifteen',
+      'one',
+      'thirty',
+      'sixty'
+    ];
+    this.state = {
+      menuIndex: 0
+    }
+  }
+
+  menuEventHandler(e, self) {
+    let def = document.querySelectorAll('.default');
+    for(var i = 0; i < def.length; i++) {
+      def[i].className = def[i].className.replace('default', '');
+    }
+    switch (e.keyCode) {
+      case 13:
+        browserHistory.push(`/write/${self.indexToURL[self.state.menuIndex]}`)
+      case 40:
+        //down
+        if (self.state.menuIndex < 3) {
+          self.setState({
+            menuIndex: (self.state.menuIndex + 1)
+          });
+        } else if (self.state.menuIndex == 3) {
+          self.setState({
+            menuIndex: 0
+          });
+        }
+        break;
+      case 38:
+        //up
+        if (self.state.menuIndex == 0) {
+          self.setState({
+            menuIndex: 3
+          });
+        } else {
+          self.setState({
+            menuIndex: (self.state.menuIndex - 1)
+          });
+        }
+        break;
+    }
+  }
+
+  componentDidMount(init = true) {
+    if (init) {
+      document.title = "Write";
+      NProgress.done();
+    }
+    document.querySelector(
+      `.${this.indexToURL[this.state.menuIndex]}`
+    ).className += ' default';
+    document.querySelector('.menu').focus();
+  }
+
+  componentDidUpdate() {
+    this.componentDidMount(false);
+  }
+
+  componentDidUnmount() {
+    window.removeEventListener('keydown');
   }
 
   render() {
     return (
-      <div className="menu fadeIn">
+      <div
+        tabIndex="0"
+        className="menu fadeIn"
+        onKeyDown={
+          (e) => {
+            this.menuEventHandler(e, this);
+          }
+        }
+        onBlur={(e) => {
+          document.querySelector('.menu').focus();
+        }}>
         <h1>Write for:</h1>
-        <Link className="menuItem" to="/write/one">1 Minute</Link>
-        <Link className="menuItem" to="/write/fifteen">15 Minutes</Link>
-        <Link className="menuItem" to="/write/thirty">30 Minutes</Link>
-        <Link className="menuItem" to="/write/sixty">60 Minutes</Link>
+        <Link className="menuItem fifteen" to="/write/fifteen">15 Minutes</Link>
+        <Link className="menuItem one" to="/write/one">1 Minute</Link>
+        <Link className="menuItem thirty" to="/write/thirty">30 Minutes</Link>
+        <Link className="menuItem sixty" to="/write/sixty">60 Minutes</Link>
         <Link className="extLink" to="/saved">/saved</Link>
         <Link className="extLink" to="/about">/about</Link>
       </div>
